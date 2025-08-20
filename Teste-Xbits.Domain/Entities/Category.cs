4f67af4189b8be262782a9baf35f140af5c0b7f7
Gabriel Morais <136760261+GMoraisz@users.noreply.github.com>
2025-08-20 09:33:54 -0300

    feat: create category and product entities

---
+++ Teste-Xbits.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Teste_Xbits.Domain.Entities;
+
+public class Category
+{
+    public long Id { get; set; }
+    public string? Name {get; set;}
+    
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ Teste-Xbits.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Teste_Xbits.Domain.Entities;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    
+    public long CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
