fa74088adcacb6e6e2a7c037acffc7ae8d58a445
Gabriel Morais <136760261+GMoraisz@users.noreply.github.com>
2025-09-13 13:16:12 -0300

    Feat: Add Repository for Product Entity (will be changed)

---
+++ Teste-Xbits.Infra/Interfaces/RepositoryContracts/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Teste_Xbits.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Teste_Xbits.Infra.Interfaces.RepositoryContracts;
+
+public interface IProductRepository : IDisposable
+{
+    Task<Product> SaveAsync(Product product);
+    Task<bool> UpdateAsync(Product product);
+    Task<bool> DeleteAsync(Product product);
+
+    Task<Product> FindByPredicateAsync(
+        Expression<Func<Product, bool>> predicate,
+        Func<IQueryable<Product>, IIncludableQueryable<Product, object>>? include = null,
+        bool asNoTracking = false);
+
+    Task<List<Product>> FindAllByPredicateAsync(
+        Expression<Func<Product, bool>> predicate,
+        Func<IQueryable<Product>, IIncludableQueryable<Product, object>>? include = null,
+        bool toQuery = false);
+    
+}
---
+++ Teste-Xbits.Infra/Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Teste_Xbits.Domain.Entities;
+using Teste_Xbits.Infra.Interfaces.RepositoryContracts;
+using Teste_Xbits.Infra.Interfaces.ServiceContracts;
+using Teste_Xbits.Infra.ORM.Context;
+using Teste_Xbits.Infra.Repositories.Base;
+
+namespace Teste_Xbits.Infra.Repositories;
+
+public sealed class ProductRepository(
+    ApplicationContext dbContext)
+    : RepositoryBase<Product>(dbContext), IProductRepository
+{
+    public async Task<Product> SaveAsync(Product product)
+    {
+        await DbSetContext.AddAsync(product);
+        await SaveInDatabaseAsync();
+
+        return product;
+    }
+
+    public Task<bool> UpdateAsync(Product product)
+    {
+        DetachedObject(product);
+        DbSetContext.Update(product);
+        return SaveInDatabaseAsync();
+    }
+
+    public Task<bool> DeleteAsync(Product product)
+    {
+        DbSetContext.Remove(product);
+        return SaveInDatabaseAsync();
+    }
+}
