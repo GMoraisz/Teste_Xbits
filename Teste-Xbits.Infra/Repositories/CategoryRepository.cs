540e11cd33b085a5a97187f5a5f6384a7fa1f0f2
Gabriel Morais <136760261+GMoraisz@users.noreply.github.com>
2025-09-13 13:17:41 -0300

    Feat: Add Repository for Category Entity (will be changed)

---
+++ Teste-Xbits.Infra/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Teste_Xbits.Domain.Entities;
+using Teste_Xbits.Infra.Interfaces.RepositoryContracts;
+using Teste_Xbits.Infra.Interfaces.ServiceContracts;
+using Teste_Xbits.Infra.ORM.Context;
+using Teste_Xbits.Infra.Repositories.Base;
+
+namespace Teste_Xbits.Infra.Repositories;
+
+public sealed class CategoryRepository(
+    ApplicationContext dbContext)
+    : RepositoryBase<Category>(dbContext), ICategoryRepository
+{
+    public async Task<bool> SaveAsync(Category category)
+    {
+        await DbSetContext.AddAsync(category);
+        return await SaveInDatabaseAsync();
+    }
+
+    public async Task<bool> UpdateAsync(Category category)
+    {
+      
+        DbSetContext.Update(category); 
+        return await SaveInDatabaseAsync();
+    }
+
+    public async Task<bool> DeleteAsync(Category category)
+    {
+        DbSetContext.Remove(category);
+        return await SaveInDatabaseAsync();
+    }
+}
