349e8057893aee20790c1ff6fac7f271e843b37c
Gabriel Morais <136760261+GMoraisz@users.noreply.github.com>
2025-09-13 13:19:34 -0300

    Feat: Add Traces for Product and Category

---
+++ Teste-Xbits.ApplicationService/Traces/CategoryTrace.cs
@@ -0,0 +1,8 @@
+﻿namespace Teste_Xbits.ApplicationService.Traces;
+
+public static class CategoryTrace
+{
+    public const string Save = "Category Register";
+    public const string Update = "Category Update";
+    public const string Delete = "Category Delete";
+}
---
+++ Teste-Xbits.ApplicationService/Traces/ProductTrace.cs
@@ -0,0 +1,8 @@
+﻿namespace Teste_Xbits.ApplicationService.Traces;
+
+public static class ProductTrace
+{
+    public const string Save = "Product Register";
+    public const string Update = "Product Update";
+    public const string Delete = "Product Delete";
+}
